cf6f8aed85a5fe3d4aa7857194368c46c5438633
Ahmad Thoriq Najahi <najahiii@outlook.co.id>
2019-09-19 18:22:36 +0700

    [update]: GCC-9.2.0-1822-190919
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>

---
+++ lib/gcc/aarch64-linux-gnu/9.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/drone/src/repo/config/.build/aarch64-linux-gnu/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-linux-gnu --prefix=/root/x-tools/aarch64-linux-gnu --with-sysroot=/root/x-tools/aarch64-linux-gnu/aarch64-linux-gnu/sysroot --enable-languages=c --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/drone/src/repo/config/.build/aarch64-linux-gnu/buildtools --with-mpfr=/drone/src/repo/config/.build/aarch64-linux-gnu/buildtools --with-mpc=/drone/src/repo/config/.build/aarch64-linux-gnu/buildtools --with-isl=/drone/src/repo/config/.build/aarch64-linux-gnu/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/root/x-tools/aarch64-linux-gnu/aarch64-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
