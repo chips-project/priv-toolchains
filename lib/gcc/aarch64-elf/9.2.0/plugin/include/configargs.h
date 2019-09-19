3441d9dd3ee23d4d400b606c96d27e05f0a0c4db
Ahmad Thoriq Najahi <najahiii@outlook.co.id>
2019-09-19 18:23:02 +0700

    [striped] [update]: GCC-9.2.0-1823-190919
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>

---
+++ lib/gcc/aarch64-elf/9.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/drone/src/repo/config/.build/aarch64-elf/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-elf --prefix=/root/x-tools/aarch64-elf --with-local-prefix=/root/x-tools/aarch64-elf/aarch64-elf --with-headers=/root/x-tools/aarch64-elf/aarch64-elf/include --with-newlib --enable-threads=no --disable-shared --enable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --with-gmp=/drone/src/repo/config/.build/aarch64-elf/buildtools --with-mpfr=/drone/src/repo/config/.build/aarch64-elf/buildtools --with-mpc=/drone/src/repo/config/.build/aarch64-elf/buildtools --with-isl=/drone/src/repo/config/.build/aarch64-elf/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-target-optspace --disable-nls --disable-multilib --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
