7558fb2bbd12561ea82cb816a1bddbe3a3358f8e
Ahmad Thoriq Najahi <najahiii@outlook.co.id>
2019-09-19 18:23:46 +0700

    [update]: GCC-9.2.0-1823-190919
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>

---
+++ lib/gcc/arm-linux-gnueabi/9.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/drone/src/repo/config/.build/arm-linux-gnueabi/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=arm-linux-gnueabi --prefix=/root/x-tools/arm-linux-gnueabi --with-sysroot=/root/x-tools/arm-linux-gnueabi/arm-linux-gnueabi/sysroot --enable-languages=c --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/drone/src/repo/config/.build/arm-linux-gnueabi/buildtools --with-mpfr=/drone/src/repo/config/.build/arm-linux-gnueabi/buildtools --with-mpc=/drone/src/repo/config/.build/arm-linux-gnueabi/buildtools --with-isl=/drone/src/repo/config/.build/arm-linux-gnueabi/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/root/x-tools/arm-linux-gnueabi/arm-linux-gnueabi/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
