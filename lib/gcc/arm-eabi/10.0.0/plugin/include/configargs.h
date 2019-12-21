6331c7b331e4b255ea9db2f55b0dbed8d6b85e40
najahiiii <najahiii@outlook.co.id>
2019-12-21 23:46:06 +0700

    [update]: GCC-10.0.0-2346-211219
    Signed-off-by: najahiiii <najahiii@outlook.co.id>

---
+++ lib/gcc/arm-eabi/10.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/drone/src/repo/config/.build/arm-eabi/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=arm-eabi --prefix=/root/x-tools/arm-eabi --with-local-prefix=/root/x-tools/arm-eabi/arm-eabi --with-headers=/root/x-tools/arm-eabi/arm-eabi/include --with-newlib --enable-threads=no --disable-shared --enable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --with-gmp=/drone/src/repo/config/.build/arm-eabi/buildtools --with-mpfr=/drone/src/repo/config/.build/arm-eabi/buildtools --with-mpc=/drone/src/repo/config/.build/arm-eabi/buildtools --with-isl=/drone/src/repo/config/.build/arm-eabi/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-target-optspace --disable-nls --disable-multilib --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" } };
